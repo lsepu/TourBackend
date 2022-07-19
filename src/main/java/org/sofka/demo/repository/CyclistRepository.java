0f1cb473667378d9576db32165a4da4d4eb9e072
lsepu <55538804+lsepu@users.noreply.github.com>
2022-07-19 15:17:49 -0500

    ADD: Repository created

---
+++ src/main/java/org/sofka/demo/repository/CyclistRepository.java
@@ -0,0 +1,9 @@
+package org.sofka.demo.repository;
+
+import org.sofka.demo.collection.Cyclist;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CyclistRepository extends ReactiveMongoRepository<Cyclist, String> {
+}
