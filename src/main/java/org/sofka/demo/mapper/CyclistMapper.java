7224fc9716f9baabe7ab62c5633d6669134aed6e
lsepu <55538804+lsepu@users.noreply.github.com>
2022-07-19 15:10:19 -0500

    ADD: Mappers added

--- src/main/java/org/sofka/demo/mapper/CyclistMapper.java
+++ src/main/java/org/sofka/demo/mapper/CyclistMapper.java
@@ -1,4 +1,24 @@
 package org.sofka.demo.mapper;
 
+import org.modelmapper.ModelMapper;
+import org.sofka.demo.collection.Cyclist;
+import org.sofka.demo.model.CyclistDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CyclistMapper {
+
+    private ModelMapper modelMapper;
+
+    public CyclistMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public CyclistDTO convertCyclistToCyclistDTO(Cyclist cyclist){
+        return modelMapper.map(cyclist, CyclistDTO.class);
+    }
+
+    public Cyclist convertCyclistDTOToCyclist(CyclistDTO cyclistDTO){
+        return modelMapper.map(cyclistDTO, Cyclist.class);
+    }
 }
--- src/main/java/org/sofka/demo/mapper/TeamMapper.java
+++ src/main/java/org/sofka/demo/mapper/TeamMapper.java
@@ -1,4 +1,27 @@
 package org.sofka.demo.mapper;
 
+import org.modelmapper.ModelMapper;
+import org.sofka.demo.collection.Team;
+import org.sofka.demo.model.TeamDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TeamMapper {
+
+    private ModelMapper modelMapper;
+
+    public TeamMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public TeamDTO convertTeamToTeamDTO(Team team){
+        return modelMapper.map(team, TeamDTO.class);
+    }
+
+    public Team convertTeamDTOToTeam(TeamDTO teamDTO){
+        return modelMapper.map(teamDTO, Team.class);
+    }
+
+
+
 }
